5bc5ef2ef25acc18fef899c43f86d01b3f5be50a
mkrohn-repo <mikron@frieswiththat.org>
2025-07-31 19:49:06 -0700

    cleaned up with error checking, and comprehensions

--- useful_python/moderized_requests/requests_no_secrets.py
+++ useful_python/moderized_requests/requests_no_secrets.py
@@ -1,44 +1,69 @@
+#something more useful, with examples of comprehensions and error handling
+# cetainly useful for inclusion in a larger project
 import requests
-
-# basic GET request cheat
-
-
-def myuserdict():
-    myuserdict = {}
-    myreq2 = requests.get("https://jsonplaceholder.typicode.com/users")
-    myusers = myreq2.json()
-    for user in myusers:
-        myuserdict.update({user["id"]: user["name"]})
-    return myuserdict
+from requests.exceptions import RequestException
+from typing import List, Dict
+API_URL = "https://jsonplaceholder.typicode.com/"
 
 
-mydict = myuserdict()
-print(f"{mydict}")
-
-# grab a "thing" from this particular api for passing to another function
-
-
-def myuser():
-    myreq0 = requests.get("https://jsonplaceholder.typicode.com/users")
-    myusers = myreq0.json()
-    for user in myusers:
-        if user["name"] == "Glenna Reichert":
-            myquest = user["id"]
-    return myquest
-
-# to get a count of some "thing"
+def get_users() -> Dict[int, str]:
+    """
+    gets users and return a dictionary {user_id: name} 
+    """
+    try:
+        url= f"{API_URL}users"
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()
+        users = response.json()
+        return {user["id"]: user["name"] for user in users} # dictionary comprehension 
+    except (RequestException, ValueError, KeyError) as e:
+        print(f"Error geting users: {e}")
+        return {}
 
 
-def myposts(myquest):
-    post_count = 0
-    myreq1 = requests.get("https://jsonplaceholder.typicode.com/posts")
-    myposts = myreq1.json()
-    for post in myposts:
-        if myquest == post['userId']:
-            post_count += 1
-    return post_count
+def get_posts() -> List[Dict]:
+    """
+    gets posts and return the raw list of post dicts
+    """
+    try:
+        url= f"{API_URL}posts"
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()
+        return response.json() # returns a list of dicts
+    except (RequestException, ValueError) as e:
+        print(f"Error geting posts: {e}")
+        return []
 
 
-myquest = myuser()
-post_count = myposts(myquest)
-print(f"postcount for userid {myquest} is {post_count}")
+def get_post_counts_by_user() -> List[Dict[str, str | int]]:
+    """
+    Return a list of dicts  {'user': 'some name', 'postcount': 100000}
+    """
+    users = get_users()
+    posts = get_posts()
+
+    if not users or not posts:
+        return []  #should not happen, but just in case
+
+    counts = {user_id: 0 for user_id in users.keys()}
+
+    for post in posts:
+        user_id = post.get("userId")
+        if user_id in counts:
+            counts[user_id] += 1
+
+    return [{"user": users[uid], "postcount": count} for uid, count in counts.items()] #list comprehension with dicts
+
+
+
+def do_the_work() -> None:
+    """
+    do the work 
+    """
+    results = get_post_counts_by_user()
+    for thing in results:
+        print(f"{thing['user']} has {thing['postcount']} posts")
+
+
+if __name__ == "__main__":
+    do_the_work()
